5ef6cdeababbbda8b74f3870e3e25a65f06454e9
libitte <libitte3@gmail.com>
2014-12-14 20:55:04 +0900

    resoleve 9.6.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :skip_signed_in_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -70,6 +71,12 @@ class UsersController < ApplicationController
       unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def skip_signed_in_user
+      if signed_in?
+        redirect_to root_url
       end
     end
 
